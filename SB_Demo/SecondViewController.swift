5074703b6f6cff0c54beab231fb14e3dc5cd64a4
Artem Ulko <ulkoart@gmail.com>
2019-10-24 14:10:00 +0300

    add second screen

---
+++ SB_Demo/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  SB_Demo
+//
+//  Created by user on 24/10/2019.
+//  Copyright © 2019 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var text: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textLabel.text = text
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SB_Demo/ViewController.swift
+++ SB_Demo/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "ShowSecondController", sender: nil)
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? SecondViewController, segue.identifier == "ShowSecondController" {
+            controller.text = "Hello, world!"
+        }
+    }    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
